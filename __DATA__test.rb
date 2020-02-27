a9025fe59e3aa44caf01823b74a0139442340e81
delightech <hisafumi.kikkawa@gmail.com>
2020-02-28 00:42:12 +0900

    modify test code

--- __DATA__test.rb
+++ __DATA__test.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-puts 'START'
-puts 'END'
-puts ''
+puts "START"
+puts
 
 DATA.each_line do |line|
   puts line
 end
 
+puts
+puts "END"
+
 __END__
 ここから下はRubyプログラムではありません。
-__END__のためのものです。
+DATA.each_lineで
+__END__以下のデータを
+改行区切りで出力したものです。
