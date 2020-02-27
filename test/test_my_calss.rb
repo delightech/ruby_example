77cba3612eaadaac8a1ba489cf447a8998a14dec
delightech <hisafumi.kikkawa@gmail.com>
2020-02-28 00:58:01 +0900

    add comment

--- test/test_my_calss.rb
+++ test/test_my_calss.rb
@@ -11,14 +11,16 @@ class TestMyClass < Test::Unit::TestCase
   # test for circleci
   # check your circleci project env and context env
   def test_test1
-    puts 'env check'
+    puts 'env check for test1'
+    # circleci上ではproject envのログはマスクされる（コンソール出力は「*****」が表示される）
     puts ENV['PROJECT_ENV_1']
     ENV['PROJECT_ENV_1'] ||= 'TEST1'
     assert_equal(ENV['PROJECT_ENV_1'], @m.test1)
   end
 
   def test_test2
-    puts 'env check'
+    puts 'env check for test2'
+    # circleci上ではproject envのログはマスクされる（コンソール出力は「*****」が表示される）
     puts ENV['PROJECT_ENV_2']
     ENV['PROJECT_ENV_2'] ||= 'TEST2'
     assert_equal(ENV['PROJECT_ENV_2'], @m.test2)
