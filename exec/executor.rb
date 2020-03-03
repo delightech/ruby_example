bb12215cdb57384c6d55eb7ddb2d79599dcff16b
delightech <hisafumi.kikkawa@gmail.com>
2020-03-03 09:25:55 +0900

    MODIFY codes

--- exec/executor.rb
+++ exec/executor.rb
@@ -1,12 +1,23 @@
 module Exec
   class Executor
-    def open
-      File.open(File.expand_path('../tmp.txt', __dir__)) { |f| puts f.readlines(chomp: true) }
+    def read
+      File.open(File.expand_path('../tmp.txt', __dir__)).readlines(chomp: true)
+    end
+    def exec
+      eval(File.open(File.expand_path('./lib/array.txt', __dir__)).read)
     end
   end
 end
 
 # $0は実行されたファイル。下記は直接このスクリプトを実行した場合に呼び出される
 if __FILE__ == $0
-  Exec::Executor.new.open
+  e = Exec::Executor.new
+  # 行を配列として読み込む
+  a = e.read
+  pp a
+  pp a[0]
+  # textをRubyプログラムとして読み込む（ハッシュの配列を生成）
+  ha = e.exec
+  pp ha
+  puts ha[0][:test]
 end
