1cd00a466be0fd3b547d0aa7c64093354d0433a9
delightech <hisafumi.kikkawa@gmail.com>
2020-03-03 09:34:21 +0900

    MODIFY codes

--- exec_file_open.rb
+++
@@ -1,4 +0,0 @@
-require './exec/executor'
-
-pp Exec::Executor.new.read
-pp Exec::Executor.new.exec
--- instance_eval_test.rb
+++
@@ -1,9 +0,0 @@
-class InstanceEvalTest
-  def initialize(&block)
-    @a = 'aaaa'
-    instance_eval(&block) if block_given?
-  end
-  def test
-    puts 'TEST!'
-  end
-end
