d591f374d9ba3da17e4f68b836dc4986d0ac538f
delightech <hisafumi.kikkawa@gmail.com>
2020-04-22 10:06:13 +0900

    MODIFY code & comment

--- heavy_api.rb
+++ heavy_api.rb
@@ -1,7 +1,7 @@
 class HeavyAPI
   def execute
-    # 30秒sleep
+    # 30秒sleep（実際はHTTPリクエストやDBアクセスなど）
     sleep(30)
-    {status: 'success'}
+    {status: 'success', results: [{status: 'success', id: 1, name: 'yamada taro'}]}
   end
 end
