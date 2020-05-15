cf07a648c6b5de2e3f20489d48717f387ba050c3
delightech <hisafumi.kikkawa@gmail.com>
2020-05-15 18:19:41 +0900

    MODIFY parse code

--- uni_unesc.rb
+++ uni_unesc.rb
@@ -1,4 +1,8 @@
 require 'json'
 unless (v = ARGV[0]).nil?
-  puts v.gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*') }
+  begin
+    puts JSON.load(v)
+  rescue
+    puts JSON.load("\"#{v}\"")
+  end
 end
